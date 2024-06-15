12a8158a3fd6412c4afdf452290ab22a40dcc4fe
Tristan Crain <tristan.crain@outlook.com>
2024-06-14 21:23:50 -0400

    added initial components

---
+++ qr-code-generator/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Link from './Link/Link'
+function App() {
+
+  return (
+    <>
+     <Link/>
+    </>
+  )
+}
+
+export default App
---
+++ qr-code-generator/src/QRCode/QRCode.jsx
@@ -0,0 +1,14 @@
+
+function QRCode(){
+
+    return(
+        <article className="qr-code-container">
+            <div className="qr-code">
+
+            </div>
+            <button>Download Now!</button>
+        </article>
+    );
+}
+
+export default QRCode
